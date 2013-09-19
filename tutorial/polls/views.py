80f59d0f15653f413997513e58fd9566cb8f57fe
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-09-19 05:53:11 +0200

    Add detail view for Poll with template

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Poll
 
@@ -6,3 +6,7 @@ def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     context = {'latest_poll_list': latest_poll_list}
     return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
